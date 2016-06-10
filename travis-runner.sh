df1cdf87eb824d8d1a3a755f882a706a304c7c7a
Manuel Braun <thembrown@gmail.com>
2016-06-10 15:02:14 +0200

    Update travis config

--- travis-runner.sh
+++ travis-runner.sh
@@ -1,11 +1,4 @@
 #!/bin/bash -e
 set -o pipefail
 
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]  && [ "$TRAVIS_NODE_VERSION" = "5.1" ]
-then
-elif [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]  && [ "$TRAVIS_NODE_VERSION" != "5.1" ]
-then
-  echo "Do Nothing, only deploy with Node 5.1"
-else
-  npm test
-fi
+npm test
